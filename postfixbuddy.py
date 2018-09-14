de006c1903820030ea3f905ae93eed80d8b3e04f
dsgnr <email@danielhand.io>
2018-09-14 20:26:28 +0100

    Added functions

--- postfixbuddy.py
+++ postfixbuddy.py
@@ -7,6 +7,8 @@ import os
 import os.path
 import argparse
 
+__version__ = '0.1.0'
+
 # Variables
 pf_dir = '/var/spool/postfix/'
 active_queue = pf_dir + 'active'
@@ -15,35 +17,48 @@ bounce_queue = pf_dir + 'bounce'
 corrupt_queue = pf_dir + 'corrupt'
 incoming_queue = pf_dir + 'incoming'
 
-parser = argparse.ArgumentParser(description="pfHandle reimagined in python.")
 
-parser.add_argument("-l", "--list", dest="list_queues",
-                    action="store_true", help="List all the current mail queues")
-parser.add_argument("-D", "--delete", dest="delete_message", action="store_true",
-                    help="Delete the email message. Options: # or \"from:\"")
-parser.add_argument("-r", "--reprocess", dest="process_queue",
-                    action="store_true", help="Try to reprocess queued messages now")
-parser.add_argument("-m", "--message", dest="display_message",
-                    action="store_true", help="Displays the message")
-parser.add_argument("-N", dest="display_message_ID",
-                    action="store_true", help="Displays only the message IDs")
-parser.add_argument("-P", dest="purge_messages", action="store_true",
-                    help="Purge all messages from the mail queue. Options: hold|incoming|active|deferred")
-parser.add_argument("-s", "--stats", dest="display_stats",
-                    action="store_true", help="Display the mail queue statistics")
-parser.add_argument("-S", "--subject", dest="select_subject", action="store_true",
-                    help="Delete mail with this subject. Options: \"$subjectname\"")
+def get_options():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--list", dest="list_queues", action="store_true",
+                        help="List all the current mail queues")
+    parser.add_argument("-D", "--delete", dest="delete_message", action="store_true",
+                        help="Delete the email message. Options: # or \"from:\"")
+    parser.add_argument("-r", "--reprocess", dest="process_queue",
+                        action="store_true", help="Try to reprocess queued messages now")
+    parser.add_argument("-m", "--message", dest="display_message",
+                        action="store_true", help="Displays the message")
+    parser.add_argument("-N", dest="display_message_ID",
+                        action="store_true", help="Displays only the message IDs")
+    parser.add_argument("-P", dest="purge_messages", action="store_true",
+                        help="Purge all messages from the mail queue. Options: hold|incoming|active|deferred")
+    parser.add_argument("-s", "--stats", dest="display_stats",
+                        action="store_true", help="Display the mail queue statistics")
+    parser.add_argument("-S", "--subject", dest="select_subject", action="store_true",
+                        help="Delete mail with this subject. Options: \"$subjectname\"")
+    version = '%(prog)s ' + __version__
+    parser.add_argument('-v', '--version', action='version', version=version)
+    return parser
 
-args = parser.parse_args()
+
+def list_queues():
+    queue_list = ['Active', 'Deferred', 'Bounce', 'Corrupt', 'Incoming']
+    queue_types = [active_queue, deferred_queue,
+                   bounce_queue, corrupt_queue, incoming_queue]
+    print
+    print '============== Mail Queue Summary =============='
+    for index in range(len(queue_list)):
+        print queue_list[index], 'Queue Count:', len([name for name in os.listdir(
+            queue_types[index]) if os.path.isfile(os.path.join(queue_types[index], name))])
+    print
+
+
+def main():
+    parser = get_options()
+    args = parser.parse_args()
+    if args.list_queues:
+        list_queues()
+
 
 if __name__ == '__main__':
-
-    if args.list_queues:
-        queue_list = ['Active', 'Deferred', 'Bounce', 'Corrupt', 'Incoming']
-        queue_types = [active_queue, deferred_queue, bounce_queue, corrupt_queue, incoming_queue]
-        print
-        print '============== Mail Queue Summary =============='
-        for index in range(len(queue_list)):
-            print queue_list[index], 'Queue Count:', len([name for name in os.listdir(
-                queue_types[index]) if os.path.isfile(os.path.join(queue_types[index], name))])
-        print
+    main()

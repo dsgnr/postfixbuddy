52adde7f5090a7093f979ef99f59a9b7bc82a32f
Dan Hand <email@danielhand.io>
2019-03-02 10:31:53 +0000

    Add linting

--- postfixbuddy.py
+++ postfixbuddy.py
@@ -3,12 +3,15 @@
 # This is a recreation of pfHandle.perl but in Python.
 
 from __future__ import absolute_import, division, print_function
+
+# Standard Library
+import argparse
 import os
 from os.path import join
-import argparse
 import sys
 import subprocess
 from subprocess import call
+
 
 __version__ = '0.1.0'
 
@@ -44,21 +47,22 @@ def get_options():
                         help='Shows version information')
     return parser
 
+
 # All variables defined in this script reply on finding the queue_directory.
-# This defines the pf_dir variable which is called later on.
+# This defines the PF_DIR variable which is called later on.
 try:
-    get_queue_dir = subprocess.Popen(['/usr/sbin/postconf',
-                                     '-h', 'queue_directory'],
+    GET_QUEUE_DIR = subprocess.Popen(['/usr/sbin/postconf',
+                                      '-h', 'queue_directory'],
                                      stdout=subprocess.PIPE,
                                      stderr=subprocess.PIPE)
-    output, error = get_queue_dir.communicate()
-    if output:
-        pf_dir = output.split()[0]
+    OUTPUT, ERROR = GET_QUEUE_DIR.communicate()
+    if OUTPUT:
+        PF_DIR = OUTPUT.split()[0]
 except OSError as ex:
     sys.exit('Unable to find Postfix queue directory!')
 
 
-class color:
+class COLOR:
     RED = '\033[31m\033[1m'
     GREEN = '\033[32m\033[1m'
     YELLOW = '\033[33m\033[1m'
@@ -70,22 +74,22 @@ class color:
 
 
 # Variables
-active_queue = pf_dir + '/active'
-bounce_queue = pf_dir + '/bounce'
-corrupt_queue = pf_dir + '/corrupt'
-deferred_queue = pf_dir + '/deferred'
-hold_queue = pf_dir + '/hold'
-incoming_queue = pf_dir + '/incoming'
-queue_list = ['Active', 'Bounce', 'Corrupt',
+ACTIVE_QUEUE = PF_DIR + '/active'
+BOUNCE_QUEUE = PF_DIR + '/bounce'
+CORRUPT_QUEUE = PF_DIR + '/corrupt'
+DEFERRED_QUEUE = PF_DIR + '/deferred'
+HOLD_QUEUE = PF_DIR + '/hold'
+INCOMING_QUEUE = PF_DIR + '/incoming'
+QUEUE_LIST = ['Active', 'Bounce', 'Corrupt',
               'Deferred', 'Hold', 'Incoming']
-queue_types = [active_queue, bounce_queue, corrupt_queue,
-               deferred_queue, hold_queue, incoming_queue]
-parser = get_options()
-args = parser.parse_args()
+QUEUE_TYPES = [ACTIVE_QUEUE, BOUNCE_QUEUE, CORRUPT_QUEUE,
+               DEFERRED_QUEUE, HOLD_QUEUE, INCOMING_QUEUE]
+PARSER = get_options()
+ARGS = PARSER.parse_args()
 
 
 def show_version():
-    print(color.GREEN + '''
+    print(COLOR.GREEN + r'''
                     _    __ _      _               _     _
     _ __   ___  ___| |_ / _(_)_  _| |__  _   _  __| | __| |_   _
     | '_ \ / _ \/ __| __| |_| \ \/ / '_ \| | | |/ _` |/ _` | | | |
@@ -93,128 +97,129 @@ def show_version():
     | .__/ \___/|___/\__|_| |_/_/\_\_.__/ \__,_|\__,_|\__,_|\__, |
     |_|                                                     |___/
 
-    version: ''' + __version__, color.RESET)
+    version: ''' + __version__, COLOR.RESET)
 
 
 def list_queues():
-    print(color.MAGENTA + '==== Mail Queue Summary ====' +
-          color.RESET)
-    for index in range(len(queue_list)):
+    print(COLOR.MAGENTA + '==== Mail Queue Summary ====' +
+          COLOR.RESET)
+    for index in range(len(QUEUE_LIST)):
         file_count = sum(len(files) for _, _, files in
-                         os.walk(queue_types[index]))
-        print(color.YELLOW + queue_list[index], 'Queue Count:' +
-              color.BLUE, file_count, color.RESET)
+                         os.walk(QUEUE_TYPES[index]))
+        print(COLOR.YELLOW + QUEUE_LIST[index], 'Queue Count:' +
+              COLOR.BLUE, file_count, COLOR.RESET)
     print
 
 
 def purge_queues():
-    print(color.RED + 'Do you really want to purge the ' +
-          args.purge_queues + ' queue? (y/n): ' + color.RESET)
+    print(COLOR.RED + 'Do you really want to purge the ' +
+          ARGS.purge_queues + ' queue? (y/n): ' + COLOR.RESET)
     tty = open('/dev/tty')
     option_answer = tty.readline().strip()
     tty.close()
     if option_answer.lower() == 'y':
-        call(['postsuper', '-d', 'ALL', args.purge_queues])
-        print(color.GREEN + 'Purged all mail from the ' +
-              args.purge_queues + ' queue!' + color.RESET)
-    if  option_answer.lower() != 'y':
-        print(color.RED + 'Exiting...' + color.RESET)
+        call(['postsuper', '-d', 'ALL', ARGS.purge_queues])
+        print(COLOR.GREEN + 'Purged all mail from the ' +
+              ARGS.purge_queues + ' queue!' + COLOR.RESET)
+    if option_answer.lower() != 'y':
+        print(COLOR.RED + 'Exiting...' + COLOR.RESET)
         sys.exit(1)
 
 
 def clean_queues():
-    print(color.RED + 'Do you really want to purge '
-          'ALL mail queues? (y/n): ' + color.RESET)
+    print(COLOR.RED + 'Do you really want to purge '
+          'ALL mail queues? (y/n): ' + COLOR.RESET)
     tty = open('/dev/tty')
     option_answer = tty.readline().strip()
     tty.close()
     if option_answer.lower() == 'y':
         call(['postsuper', '-d', 'ALL'])
-        print(color.GREEN + 'Purged all mail queues!' + color.RESET)
-    if  option_answer.lower() != 'y':
-        print(color.RED + 'Exiting...' + color.RESET)
+        print(COLOR.GREEN + 'Purged all mail queues!' + COLOR.RESET)
+    if option_answer.lower() != 'y':
+        print(COLOR.RED + 'Exiting...' + COLOR.RESET)
         sys.exit(1)
 
 
 def delete_mail():
-    print(color.RED + 'Do you really want to delete mail ' +
-          args.delete_mail + '? (y/n): ' +
-          color.RESET)
+    print(COLOR.RED + 'Do you really want to delete mail ' +
+          ARGS.delete_mail + '? (y/n): ' +
+          COLOR.RESET)
     tty = open('/dev/tty')
     option_answer = tty.readline().strip()
     tty.close()
     if option_answer.lower() == 'y':
-        call(['postsuper', '-d', args.delete_mail])
-        print(color.GREEN + 'Deleted mail ID: ' + color.YELLOW +
-              args.delete_mail + color.GREEN + '!' + color.RESET)
-    if  option_answer.lower() != 'y':
-        print(color.RED + 'Exiting...' + color.RESET)
+        call(['postsuper', '-d', ARGS.delete_mail])
+        print(COLOR.GREEN + 'Deleted mail ID: ' + COLOR.YELLOW +
+              ARGS.delete_mail + COLOR.GREEN + '!' + COLOR.RESET)
+    if option_answer.lower() != 'y':
+        print(COLOR.RED + 'Exiting...' + COLOR.RESET)
         sys.exit(1)
 
 
 def hold_queues():
     call(['postsuper', '-h', 'ALL'])
-    print(color.GREEN + 'All mail queues now on hold!' + color.RESET)
+    print(COLOR.GREEN + 'All mail queues now on hold!' + COLOR.RESET)
 
 
 def release_queues():
     call(['postsuper', '-H', 'ALL'])
-    print(color.GREEN + 'Queues no longer in a held state!' + color.RESET)
+    print(COLOR.GREEN + 'Queues no longer in a held state!' + COLOR.RESET)
 
 
 def process_queues():
     call(['postqueue', '-f'])
-    print(color.GREEN + 'Flushed all queues!' + color.RESET)
+    print(COLOR.GREEN + 'Flushed all queues!' + COLOR.RESET)
 
 
 def show_message():
-    call(['postcat', '-q', args.show_message])
+    call(['postcat', '-q', ARGS.show_message])
 
 
 def delete_by_search():
     count = 0
-    for index in range(len(queue_list)):
-        for (dirname, dirs, files) in os.walk(queue_types[index]):
+    for index in range(len(QUEUE_LIST)):
+        for (dirname, dirs, files) in os.walk(QUEUE_TYPES[index]):
             for mail_id in files:
                 thefile = os.path.join(dirname, mail_id)
                 for line in open(thefile):
-                    if args.delete_by_search in line:
+                    if ARGS.delete_by_search in line:
                         subprocess.Popen(['/usr/sbin/postsuper',
-                                         '-d', mail_id],
+                                          '-d', mail_id],
                                          stdout=subprocess.PIPE,
                                          stderr=subprocess.PIPE)
                         count += 1
-    print(color.BLUE + 'Looking for mail containing: \"' +
-          args.delete_by_search + '\"...' + color.RESET)
+    print(COLOR.BLUE + 'Looking for mail containing: \"' +
+          ARGS.delete_by_search + '\"...' + COLOR.RESET)
     if count == 0:
-        print(color.RED + '\"' + args.delete_by_search +
-              '\" not found in search.' + color.RESET)
+        print(COLOR.RED + '\"' + ARGS.delete_by_search +
+              '\" not found in search.' + COLOR.RESET)
     if count != 0:
-        print(color.GREEN + 'Total deleted: {0}'.format(count) + color.RESET)
+        print(COLOR.GREEN + 'Total deleted: {0}'.format(count) + COLOR.RESET)
 
 
 def main():
-    if args.show_version:
+    if ARGS.show_version:
         return show_version()
-    if args.list_queues:
+    if ARGS.list_queues:
         return list_queues()
-    if args.purge_queues:
+    if ARGS.purge_queues:
         return purge_queues()
-    if args.clean_queues:
+    if ARGS.clean_queues:
         return clean_queues()
-    if args.delete_mail:
+    if ARGS.delete_mail:
         return delete_mail()
-    if args.hold_queues:
+    if ARGS.hold_queues:
         return hold_queues()
-    if args.release_queues:
+    if ARGS.release_queues:
         return release_queues()
-    if args.process_queues:
+    if ARGS.process_queues:
         return process_queues()
-    if args.show_message:
+    if ARGS.show_message:
         return show_message()
-    if args.delete_by_search:
+    if ARGS.delete_by_search:
         return delete_by_search()
     return list_queues()
+
 
 if __name__ == '__main__':
     main()
